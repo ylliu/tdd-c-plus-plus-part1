0fc0aabd1c3e283f19f2debb39c0f49355860cb6
yyy <1232423@xx.com>
2023-03-21 16:34:12 +0800

    Enhancing a test and getting another pass

--- Test.h
+++ Test.h
@@ -22,6 +22,10 @@ namespace MereTDD {
 
         virtual ~TestBase() = default;
 
+        virtual void runEx ()
+        {
+            run();
+        }
         virtual void run() = 0;
 
         std::string_view name() const {
@@ -36,17 +40,40 @@ namespace MereTDD {
             return mReason;
         }
 
+        std::string_view expectedReason () const
+        {
+            return mExpectedReason;
+        }
         void setFailed(std::string_view reason) {
             mPassed = false;
             mReason = reason;
+        }
+
+        void setExpectedFailureReason (std::string_view reason)
+        {
+            mExpectedReason = reason;
         }
 
     private:
         std::string mName;
         bool mPassed;
         std::string mReason;
+        std::string mExpectedReason;
     };
 
+    class MissingException
+    {
+    public:
+        MissingException (std::string_view exType)
+                : mExType(exType)
+        { }
+        std::string_view exType () const
+        {
+            return mExType;
+        }
+    private:
+        std::string mExType;
+    };
     inline std::vector<TestBase *> &getTests() {
         static std::vector<TestBase *> tests;
         return tests;
@@ -58,34 +85,66 @@ namespace MereTDD {
                << " tests\n";
 
         int numPassed = 0;
+        int numMissedFailed = 0;
         int numFailed = 0;
         for (auto *test: getTests()) {
             output << "---------------\n"
                    << test->name() << "\n";
-            try {
-                test->run();
+            try{
+                test->runEx();
             }
-            catch (...) {
+            catch (MissingException const & ex){
+                std::string message = "Expected exception type ";
+                message += ex.exType();
+                message += " was not thrown.";
+                test->setFailed(message);
+            }
+            catch (...){
                 test->setFailed("Unexpected exception thrown.");
             }
-            if (test->passed()) {
+            if (test->passed())
+            {
+                if (not test->expectedReason().empty())
+                {
+                    // This test passed but it was supposed
+                    // to have failed.
+                    ++numMissedFailed;
+                    output << "Missed expected failure\n"
+                           << "Test passed but was expected to fail."
+                           << std::endl;
+                }
+                else
+                {
+                    ++numPassed;
+                    output << "Passed"
+                           << std::endl;
+                }
+            }
+            else if (not test->expectedReason().empty() &&
+                     test->expectedReason() == test->reason())
+            {
                 ++numPassed;
-                output << "Passed\n";
-            } else {
+                output << "Expected failure\n"
+                       << test->reason()
+                       << std::endl;
+            }
+            else
+            {
                 ++numFailed;
                 output << "Failed\n"
                        << test->reason()
-                       << "\n";
+                       << std::endl;
             }
         }
 
         output << "---------------\n";
-        if (numFailed == 0) {
-            output << "All tests passed.\n";
-        } else {
-            output << "Tests passed: " << numPassed
-                   << "\nTests failed: " << numFailed << "\n";
+        output << "Tests passed: " << numPassed
+               << "\nTests failed: " << numFailed;
+        if (numMissedFailed != 0)
+        {
+            output << "\nTests failures missed: " <<         numMissedFailed;
         }
+        output << std::endl;
         return numFailed;
     }
 } // namespace MereTDD
@@ -104,4 +163,31 @@ public: \
 }; \
 MERETDD_CLASS MERETDD_INSTANCE(testName); \
 void MERETDD_CLASS::run ()
+
+#define TEST_EX( testName, exceptionType ) \
+class MERETDD_CLASS : public MereTDD::TestBase \
+{ \
+public: \
+    MERETDD_CLASS (std::string_view name) \
+    : TestBase(name) \
+    { \
+        MereTDD::getTests().push_back(this); \
+    } \
+    void runEx () override \
+    { \
+        try \
+        { \
+            run(); \
+        } \
+        catch (exceptionType const &) \
+        { \
+            return; \
+        } \
+        throw MereTDD::MissingException(#exceptionType); \
+    } \
+    void run () override; \
+}; \
+MERETDD_CLASS MERETDD_INSTANCE(testName); \
+void MERETDD_CLASS::run ()
+
 #endif // MERETDD_TEST_H
--- tests/Creation.cpp
+++ tests/Creation.cpp
@@ -5,6 +5,32 @@ TEST("Test can be created"){
 
 };
 
-TEST("Test with throw can be created") {
+TEST("Test that throws unexpectedly can be created")
+{
+    setExpectedFailureReason(
+            "Unexpected exception thrown.");
+    throw "Unexpected";
+}
+
+TEST_EX("Test with throw can be created",int) {
     throw 1;
+}
+
+TEST_EX("Test that never throws can be created", int)
+{
+    setExpectedFailureReason(
+            "Expected exception type int was not thrown.");
+}
+
+TEST_EX("Test that throws wrong type can be created", int)
+{
+    setExpectedFailureReason(
+            "Unexpected exception thrown.");
+    throw "Wrong type";
+}
+
+TEST("Test that should throw unexpectedly can be created")
+{
+    setExpectedFailureReason(
+            "Unexpected exception thrown.");
 }
